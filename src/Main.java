58150c083ca0e44d4bc47bcd683b1cc00e1f3504
Trayan Todorov <107070581+BoomStickk@users.noreply.github.com>
2023-05-09 17:58:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!,HI");
-         System.out.println("fixing bug");
+         System.out.print("fixing bug");
     }
 }
